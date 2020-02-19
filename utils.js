9508d8a7b624ba71ceda62810967e869712e906f
Alexander Arce <alexander@cirici.com>
2020-02-19 16:24:47 +0100

    Updated path join to take more arguments

--- utils.js
+++ utils.js
@@ -5,18 +5,18 @@ const os = require("os");
 module.exports = {
   sortFiles: (extension, date, fileName, originalPath) => {
     let newPath;
-    const year = new Date(date).getFullYear();
-    const datedFileName = `${year}/${fileName}`;
+    const year = new Date(date).getFullYear().toString();
 
     switch (extension) {
       case ".jpg":
       case ".png":
-        newPath = path.join(os.homedir(), `Pictures/${datedFileName}`);
+        newPath = path.join(os.homedir(), "Pictures", year, fileName);
         break;
       default:
         newPath = null;
         break;
     }
+
     if (newPath) {
       fs.move(originalPath, newPath, err => {
         if (err) return console.error(err);
