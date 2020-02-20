0af8d347f29b562b74e9a29ff35650915fe86b14
Alexander Arce <alexander@cirici.com>
2020-02-20 10:52:06 +0100

    Added commander

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+#!/usr/bin/env node
+
 const path = require("path");
 const fs = require("fs");
+const os = require("os");
+const program = require("commander");
 const { getDesktopFolder } = require("platform-folders");
 
 const utils = require("./utils");
-const args = process.argv.slice(2);
+
+// Commander
+program.version(require("./package.json").version);
+
+program
+  .option("-d, --debug", "Output extra debugging")
+  .option("-D, --Directory <path>", "The directory path from the root folder")
+  .option("-w, --watch", "Enable watch mode");
+
+program.parse(process.argv);
+
 // Gets first argument as directory name, if not specified default to Desktop
-const directoryPath = path.join(getDesktopFolder(), args[0] || "Desktop");
+const directoryPath = program.Directory
+  ? path.join(os.homedir(), program.Directory)
+  : getDesktopFolder();
 
 // Reads the directory and searches for files
 fs.readdir(directoryPath, function(err, files) {
